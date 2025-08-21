febe25cc9c51ffc3731e6b76b1cf03c6d0d6dfeb
santosh1231we <sitesblogger81@gmail.com>
2025-08-21 18:02:52 +0530

    chore

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -7,20 +7,10 @@ export default function Footer() {
     <footer className="container py-6 md:px-8 md:py-0">
       <div className="flex flex-col items-end justify-between md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built by{" "}
+          Built by <span className="font-medium">J Santosh Raj</span>. The source code is available on{" "}
           <Link
             prefetch={false}
-            href="https://twitter.com/DariusLukasukas"
-            target="_blank"
-            rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            Darius Lukasukas
-          </Link>
-          . The source code is available on{" "}
-          <Link
-            prefetch={false}
-            href="https://github.com/DariusLukasukas/stocks"
+            href="https://github.com/santosh1231we/Stock-game"
             target="_blank"
             rel="noreferrer"
             className="font-medium underline underline-offset-4"
